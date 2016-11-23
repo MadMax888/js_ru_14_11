722f2849a0e9866bb28015ad88c61fc0fa289d48
Roman <kaboom.ua@gmail.com>
2016-11-23 21:25:24 +0200

    Update accordionArticles.js

--- src/decorators/accordionArticles.js
+++ src/decorators/accordionArticles.js
@@ -8,6 +8,7 @@ export default (Component) => class AccordionArticles extends React.Component {
 		// 		}
 		// }
 		state = { // ES7 set default state
+				//не привязывайся к названиям сущностей в декораторах, вся их суть в том, чтобы использовать с разными компонентами и данными. Сделай openItemId
 				openArticleId: null
 		}
 
