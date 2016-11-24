6a416b31ac25755e2147eb19ee136ce21fa6a03b
MadMax888 <maxim_ivashchuk@ukr.net>
2016-11-24 12:24:59 +0200

    do statelles component form ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,32 +2,62 @@ import React, { Component, PropTypes }  from 'react'
 import Article from './Article'
 import Accordion from '../decorators/accordion'
 
-class ArticleList extends Component {
-    static propTypes = {
-       articles : PropTypes.array
-      ,openArticleId : PropTypes.string
-      ,openArticle  : PropTypes.func
-    }
 
-    render() {
-        const { articles, openItemId, openItem } = this.props
-        const articleItems = articles.map(article => (
-            <li key = {article.id}>
-                <Article
-                    article = {article}
-                    isOpen = {article.id == openItemId}
-                    toggleOpen = {openItem(article.id)}
-                />
-            </li>
-        ))
+function ArticleList (props) { // optimization to stateless component
+  const { articles, openItemId, openItem } = props
 
-        return (
-            <ul>
-                {articleItems}
-            </ul>
-        )
-    }
-
+  return (
+    <ul>
+      {getArticleItems(props)}
+    </ul>
+  )
 }
+ArticleList.propTypes = {
+   articles   : PropTypes.array
+  ,openItemId : PropTypes.string
+  ,openItem   : PropTypes.func
+}
+
+function getArticleItems(props) {
+    const { articles, openItemId, openItem } = props
+    const articleItems = articles.map(article => (
+        <li key = {article.id}>
+            <Article
+                article = {article}
+                isOpen = {article.id == openItemId}
+                toggleOpen = {openItem(article.id)}
+            />
+        </li>
+    ))
+    return articleItems
+}
+
+// class ArticleList extends Component { // es6 comment
+//     static propTypes = {
+//        articles   : PropTypes.array
+//       ,openItemId : PropTypes.string
+//       ,openItem   : PropTypes.func
+//     }
+//
+//     render() {
+//         const { articles, openItemId, openItem } = this.props
+//         const articleItems = articles.map(article => (
+//             <li key = {article.id}>
+//                 <Article
+//                     article = {article}
+//                     isOpen = {article.id == openItemId}
+//                     toggleOpen = {openItem(article.id)}
+//                 />
+//             </li>
+//         ))
+//
+//         return (
+//             <ul>
+//                 {articleItems}
+//             </ul>
+//         )
+//     }
+//
+// }
 
 export default Accordion(ArticleList)
