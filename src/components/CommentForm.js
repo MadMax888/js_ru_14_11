67135fb93bf4808734786dba0ab54040d9c85533
MadMax888 <maxim_ivashchuk@ukr.net>
2016-11-28 10:00:15 +0200

    complited dz PS if it's to late, see prev commit

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -7,14 +7,17 @@ class CommentForm extends Component {
 
 	static propTypes = {
 		 textVal : PropTypes.string.isRequired
+		,userName : PropTypes.string.isRequired
 	}
 
 	static defaultProps = {
-		 textVal : ""
+			textVal : ""
+		 ,userName : ""
 	}
 
 	state = {
-		 textVal : ""
+		  textVal : ""
+		 ,titleVal : ""
 	}
 
 	componentWillReceiveProps() {
@@ -24,25 +27,39 @@ class CommentForm extends Component {
 	componentWillUpdate() {
 			//console.log('---', 'CL will update')
 	}
-
-	setValueText = text => {
-			this.setState({
-					textVal: text
-			})
+	// Не правильно понял дз, доделал немного! Знаю что уже поздно, но все же
+	handleChange = stateValue => event => {
+		this.setState({
+				[stateValue] : event.target.value
+		})
 	}
 
-	handleChange = event => {
-		this.setValueText(event.target.value)
+	getUserName () {
+		const {userName} = this.props
+		let user = null
+		if (userName === "") {
+			user = (<p><span>User &nbsp;</span><strong>Anon</strong></p>)
+		} else {
+			user = (<p><strong>{userName}</strong></p>)
+		}
+		return user
 	}
-
 	render () {
+
 		return (
 			<form method="get" action="#" name="CommentForm">
-				<label htmlFor="inputTextArea_1">Add your comment  </label>
-					<input type="textarea" rows="5"
+					{this.getUserName()}
+					<label htmlFor="inputUserName_1">Имя &nbsp;</label>
+					<input type="text" id="inputUserName_1" name="title"
+								value={this.state.titleVal}
+								onChange={this.handleChange('titleVal')}
+							/>
+					<br />
+					<label htmlFor="inputTextArea_1">Add your comment  &nbsp;</label>
+					<input type="textarea" rows="5" cols="10" name="comment"
 								 id="inputTextArea_1"
 								 value={this.state.textVal}
-								onChange={this.handleChange}
+								onChange={this.handleChange('textVal')}
 							/>
 			</form>
 		)
