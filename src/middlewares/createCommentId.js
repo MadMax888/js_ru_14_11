a9a852c814e72ccf8aa38a3a9a1b502c933f6956
Roman <kaboom.ua@gmail.com>
2016-12-07 22:24:49 +0200

    Update createCommentId.js

--- src/middlewares/createCommentId.js
+++ src/middlewares/createCommentId.js
@@ -6,9 +6,10 @@ export default store => next => action => {
     // console.log('---', 'dispatching', action)
     const {articles, comments} = store.getState()
     const { type, payload } = action
-
+    //лучше придумать более общий способ; через мидлвару будут проходить все экшины, их стоит делать максимально реюзабельными
     switch (type) {
       case ADD_COMMENT:
+        //Плохая логика для генерации id
         const newCommentId = comments.toArray().length
 
         next({
