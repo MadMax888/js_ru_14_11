81c0a5628294263c049da293e1b7f162d0b0feb9
Roman <kaboom.ua@gmail.com>
2016-12-18 18:28:31 +0200

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react'
 
 function Loader(props,context) {
+    //общее замечание ко всем остальным местам - лучше выносить эту логику в отдельные компоненты, например LocalizedText,
+    //которые будут прятать этот механизм, по примеру Link
     const { loaderTitle } = context.localization.dictionary[context.localization.checkedLng]
 
     return (
